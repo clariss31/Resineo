927316959ac77840517d284a966b838867a17a66
Clarisse Ferand <clarisse.ferand@gmail.com>
2025-11-28 10:33:11 +0100

    Implémentation de la base de la structure MVC

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once 'config/config.php';
+require_once 'config/autoload.php';
+
+// On récupère l'action demandée par l'utilisateur.
+// Si aucune action n'est demandée, on affiche la page d'accueil.
+$action = Utils::request('action', 'home');
+
+// Try catch global pour gérer les erreurs
+try {
+    // Pour chaque action, on appelle le bon contrôleur et la bonne méthode.
+    switch ($action) {
+        // Pages accessibles à tous.
+        case 'home':
+            echo "Page d'accueil (à implémenter)";
+            break;
+
+        default:
+            throw new Exception("La page demandée n'existe pas.");
+    }
+} catch (Exception $e) {
+    // En cas d'erreur, on affiche la page d'erreur.
+    echo "Erreur : " . $e->getMessage();
+}
